b0e4e84d42d615e4bfaadb9a35444dcd1afad8b5
tazbin <mdtazbinur@gmail.com>
2023-11-23 09:41:09 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <header>
+    <div>
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+
+  <script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from './components/HelloWorld.vue'
+  </script>
