5d239a36f616d62d356cf17bb241d4a7fda55036
tazbin <mdtazbinur@gmail.com>
2023-11-24 18:58:28 +0600

    feat: element plus added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>
-    <router-link to="/signup">Signup</router-link>
+  <div class="container mx-auto">
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+  >
+    <el-menu-item index="0">
+      <h1 class="text-xl">LET'S BLOG</h1>
+    </el-menu-item>
+    <div class="flex-1" />
+
+    <el-menu-item index="1">
+      <router-link to="/signup">Signup</router-link>
+    </el-menu-item>
+    <el-menu-item index="2">
     <router-link to="/signin">Signin</router-link>
-    <router-link to="/blogs">Blogs</router-link>
-    <router-link to="/news">News</router-link>
-    <router-link to="/contact">Contact</router-link>
-  </div>
+    </el-menu-item>
+    <el-menu-item index="3">
+    <router-link to="/">Blogs</router-link>
+    </el-menu-item>
+  </el-menu>
+</div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+</script>
--- src/views/BlogsPage.vue
+++ src/views/BlogsPage.vue
@@ -5,5 +5,29 @@
     corrupti. Sapiente debitis fugiat impedit neque at? Sequi, corporis! Aut odit laboriosam
     exercitationem necessitatibus dolor reprehenderit.
     <hr />
+
+    <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+
+  <div class="flex">
+    <el-button type="primary" :icon="Edit" />
+    <el-button type="primary" :icon="Share" />
+    <el-button type="primary" :icon="Delete" />
+    <el-button type="primary" :icon="Search">Search</el-button>
+    <el-button type="primary">
+      Upload<el-icon class="el-icon--right"><Upload /></el-icon>
+    </el-button>
+  </div>
+
   </div>
 </template>
+
+<script setup>
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
+</script>
